9184f29ca2907aee79941a8055ae7b547e54e9e6
Luis Abanto <lfam200@hotmail.com>
2015-09-30 16:47:00 -0500

    gota

--- resources/views/water.blade.php
+++ resources/views/water.blade.php
@@ -7,6 +7,25 @@
             
         </div>
     </div>
+    <div class="blue">
+        <div class="container">
+            <div class="row">
+            	<div class="col-xs-4">
+                    <img src="{{ asset('img/gota.png') }}" alt="Water Planet" width="100%" class="center-block" >
+                </div>
+                <div class="col-xs-8">
+                    <h2 class="toilet text-left visible-lg">It wouldn't be possible to make a rave with no toilets or sinks. Would you like to join us in providing the dignity of a toilet for those people in developing countries</h2>
+                    <h3 class="toilet text-left visible-sm visible-md">It wouldn't be possible to make a rave with no toilets or sinks. Would you like to join us in providing the dignity of a toilet for those people in developing countries</h3>
+                    <h3 class="toilet text-left visible-xs">It wouldn't be possible to make a rave with no toilets or sinks. Would you like to join us in providing the dignity of a toilet for those people in developing countries</h3>
+                    <div class="col-xs-12 text-center margin-bottom">
+                        <button type="button" class="btn btn-success btn-lg ">Help a Warrior</button>
+                    </div>
+                </div>
+                
+                 
+            </div>
+        </div>
+    </div>
    
 
 @stop
