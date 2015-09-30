3402da53980fa5fd10e5ade0ad4728f5d9ef79e5
Luis Abanto <lfam200@hotmail.com>
2015-09-30 17:12:39 -0500

    gota1

--- resources/views/water.blade.php
+++ resources/views/water.blade.php
@@ -11,14 +11,15 @@
         <div class="container">
             <div class="row">
             	<div class="col-xs-4">
-                    <img src="{{ asset('img/gota.png') }}" alt="Water Planet" width="100%" class="center-block" >
+                    <img src="{{ asset('img/gota.png') }}" alt="Water Planet" width="50%" class="center-block" >
                 </div>
                 <div class="col-xs-8">
-                    <h2 class="toilet text-left visible-lg">It wouldn't be possible to make a rave with no toilets or sinks. Would you like to join us in providing the dignity of a toilet for those people in developing countries</h2>
-                    <h3 class="toilet text-left visible-sm visible-md">It wouldn't be possible to make a rave with no toilets or sinks. Would you like to join us in providing the dignity of a toilet for those people in developing countries</h3>
+                    <h2 class="toilet text-left visible-lg">Planet Water</h2>
+                    <h3 class="toilet text-left visible-sm visible-md">Planet Water</h3>
                     <h3 class="toilet text-left visible-xs">It wouldn't be possible to make a rave with no toilets or sinks. Would you like to join us in providing the dignity of a toilet for those people in developing countries</h3>
                     <div class="col-xs-12 text-center margin-bottom">
-                        <button type="button" class="btn btn-success btn-lg ">Help a Warrior</button>
+                        <p class="text-justify">This planet and all who inhabit in it, are made by water, since 71% of our surface (oceans, rivers, glaciers), 70% of our bodies and even what we eat, wear and do. In short words the water is hidden even in our thoughts. <br>Of this 71%:  97% is salt (oceans) and the remaining 3% (drinkable) distribute it in:     Agriculture 70%  - 22% industry - 8% to cover our basic needs. <br>100 years ago, big cities like Rotterdam, London, Montereal, Paris, New York, Buenos Aires, Shanghai, Tokyo, Chicago, and Hong Kong were centers infectious diseases. The death rate in children was the same that exists today in Africa. In general, water and sanitation reforms were those that allowed humans to progress forward.
+</p>
                     </div>
                 </div>
                 
