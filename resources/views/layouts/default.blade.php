d4724e0cabd81246d769002cc47d3be42ac52e9a
Luis Abanto <lfam200@hotmail.com>
2015-09-30 09:46:38 -0500

    logo

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -108,7 +108,7 @@
             </div>
             <div class="row">
                 <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8"><h4>International Ravers is a 501 (c)(3) • privacy policy • ©2015 • made with ❤ in CA. </h4></div>
-                <div class="col-xs-6 col-xs-offset-3 col-sm-6 col-sm-offset-3 col-md-1 col-lg-1"> <img src="{{  asset('img/oneclick.png')  }}" alt="One Click Peru SAC" width="100%"></div>
+                <div class="col-xs-4 col-xs-offset-4 col-sm-4 col-sm-offset-4 col-md-1 col-lg-1"> <img src="{{  asset('img/oneclick.png')  }}" alt="One Click Peru SAC" width="100%"></div>
                 
                
             </div>
