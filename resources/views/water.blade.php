8e49946d7828be2300e315a2c510e5274b907865
Luis Abanto <lfam200@hotmail.com>
2015-09-30 16:06:12 -0500

    inicio water

--- resources/views/water.blade.php
+++ resources/views/water.blade.php
@@ -1,13 +1,8 @@
 @extends('layouts.default')
 
 @section('content')
-    <div class="fullscreen-bg">
-        <video loop muted autoplay poster="img/videoframe.jpg" class="fullscreen-bg__video">
-            <source src="video/heroes_short.mp4" type="video/mp4">
-            
-        </video>
-    </div>
-    <div class="video">
+    
+    <div class="bg_water">
         <div class="container">
             <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2 ">
                 <h2 class="textvideo visible-lg">International Ravers Charity represents the EDM COMMUNITY worldwide facing the main problem in this planet now a days: WATER  SCARCITY IN DEVELOPING COUNTRIES</h2>
