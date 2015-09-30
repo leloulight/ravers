690170935cda6a6d4134ab34c5f3c7c46f2f6d31
Luis Abanto <lfam200@hotmail.com>
2015-09-30 17:16:09 -0500

    gota2

--- resources/views/water.blade.php
+++ resources/views/water.blade.php
@@ -14,13 +14,12 @@
                     <img src="{{ asset('img/gota.png') }}" alt="Water Planet" width="50%" class="center-block" >
                 </div>
                 <div class="col-xs-8">
-                    <h2 class="toilet text-left visible-lg">Planet Water</h2>
+                    <h2 class="p_water text-left visible-lg">Planet Water</h2>
                     <h3 class="toilet text-left visible-sm visible-md">Planet Water</h3>
-                    <h3 class="toilet text-left visible-xs">It wouldn't be possible to make a rave with no toilets or sinks. Would you like to join us in providing the dignity of a toilet for those people in developing countries</h3>
-                    <div class="col-xs-12 text-center margin-bottom">
-                        <p class="text-justify">This planet and all who inhabit in it, are made by water, since 71% of our surface (oceans, rivers, glaciers), 70% of our bodies and even what we eat, wear and do. In short words the water is hidden even in our thoughts. <br>Of this 71%:  97% is salt (oceans) and the remaining 3% (drinkable) distribute it in:     Agriculture 70%  - 22% industry - 8% to cover our basic needs. <br>100 years ago, big cities like Rotterdam, London, Montereal, Paris, New York, Buenos Aires, Shanghai, Tokyo, Chicago, and Hong Kong were centers infectious diseases. The death rate in children was the same that exists today in Africa. In general, water and sanitation reforms were those that allowed humans to progress forward.
-</p>
-                    </div>
+                    <h3 class="toilet text-left visible-xs">Planet Water</h3>
+                    
+                    <p class="text-justify texto-blanco">This planet and all who inhabit in it, are made by water, since 71% of our surface (oceans, rivers, glaciers), 70% of our bodies and even what we eat, wear and do. In short words the water is hidden even in our thoughts. <br>Of this 71%:  97% is salt (oceans) and the remaining 3% (drinkable) distribute it in:     Agriculture 70%  - 22% industry - 8% to cover our basic needs. <br>100 years ago, big cities like Rotterdam, London, Montereal, Paris, New York, Buenos Aires, Shanghai, Tokyo, Chicago, and Hong Kong were centers infectious diseases. The death rate in children was the same that exists today in Africa. In general, water and sanitation reforms were those that allowed humans to progress forward.</p>
+                    
                 </div>
                 
                  
