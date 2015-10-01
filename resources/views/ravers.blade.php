da18f73771d7211be97e30b6574c819f2bf7a52a
Luis Abanto <lfam200@hotmail.com>
2015-09-30 22:38:20 -0500

    ravers section a

--- resources/views/ravers.blade.php
+++ resources/views/ravers.blade.php
@@ -4,24 +4,21 @@
     
   <div class="bg_ravers">
   </div>
-  <div class="blue">
-        <div class="container">
-            <div class="row">
-            	<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-                    <img src="{{ asset('img/gota.png') }}" alt="Water Planet" width="60%" class="center-block vertical" >
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
-                    <h2 class="toilet text-left visible-lg">Planet Water</h2>
-                    <h3 class="toilet text-left visible-sm visible-md">Planet Water</h3>
-                    <h3 class="toilet text-left visible-xs">Planet Water</h3>
-                    
-                    <p class="text-justify texto-blanco margin-bottom-lg">This planet and all who inhabit in it, are made by water, since 71% of our surface (oceans, rivers, glaciers), 70% of our bodies and even what we eat, wear and do. In short words the water is hidden even in our thoughts. <br><br>Of this 71%:  97% is salt (oceans) and the remaining 3% (drinkable) distribute it in:     Agriculture 70%  - 22% industry - 8% to cover our basic needs. <br><br>100 years ago, big cities like Rotterdam, London, Montereal, Paris, New York, Buenos Aires, Shanghai, Tokyo, Chicago, and Hong Kong were centers infectious diseases. The death rate in children was the same that exists today in Africa. In general, water and sanitation reforms were those that allowed humans to progress forward.</p>
-                    
-                </div>
-                
-                 
-            </div>
-        </div>
+  <div class="green">
+    <div class="container">
+      <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2 ">
+          <h2 class="textvideo visible-lg">“WE ALL  TOGETHER CAN MAKE A WORLD BETTER”</h2>
+          <h3 class="textvideo visible-sm visible-md">“WE ALL  TOGETHER CAN MAKE A WORLD BETTER”</h3>
+
+          <h3 class="textvideo visible-xs bg-imagen">“WE ALL  TOGETHER CAN MAKE A WORLD BETTER”</h3>
+          <p class="text-center text-standar">If we say that  the internet  pushed the development of this culture, we are not wrong, but if we look beyond our "Smartphones" we can realize that this culture and this industry grew of hand long before the internet and mobile devices were affordable worldwide. It grew thanks to easy access to water both for drinking and hygiene for use in bathrooms.</p>
+      </div>
+
+      <div class="col-xs-12 text-center">
+          <button type="button" class="btn btn-success btn-lg textvideo">WATCH THE VIDEO</button>
+      </div>
     </div>
+  </div>
+
 
 @stop
