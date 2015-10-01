c058b7294c2172ef30ba95d502ea9f4b8781a5c4
Luis Abanto <lfam200@hotmail.com>
2015-09-30 22:51:28 -0500

    ravers text

--- resources/views/ravers.blade.php
+++ resources/views/ravers.blade.php
@@ -11,12 +11,10 @@
           <h3 class="textvideo visible-sm visible-md">“WE ALL  TOGETHER CAN MAKE A WORLD BETTER”</h3>
 
           <h3 class="textvideo visible-xs bg-imagen">“WE ALL  TOGETHER CAN MAKE A WORLD BETTER”</h3>
-          <p class="text-center text-standar">If we say that  the internet  pushed the development of this culture, we are not wrong, but if we look beyond our "Smartphones" we can realize that this culture and this industry grew of hand long before the internet and mobile devices were affordable worldwide. It grew thanks to easy access to water both for drinking and hygiene for use in bathrooms.</p>
+          <p class="text-center text-standar texto-blanco">If we say that  the internet  pushed the development of this culture, we are not wrong, but if we look beyond our "Smartphones" we can realize that this culture and this industry grew of hand long before the internet and mobile devices were affordable worldwide. It grew thanks to easy access to water both for drinking and hygiene for use in bathrooms.</p>
       </div>
 
-      <div class="col-xs-12 text-center">
-          <button type="button" class="btn btn-success btn-lg textvideo">WATCH THE VIDEO</button>
-      </div>
+     
     </div>
   </div>
 
