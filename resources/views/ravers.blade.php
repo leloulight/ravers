fb5c635b4c2bccf3677675062b059fea77a303d0
Luis Abanto <lfam200@hotmail.com>
2015-10-01 12:45:42 -0500

    ravers v2

--- resources/views/ravers.blade.php
+++ resources/views/ravers.blade.php
@@ -7,10 +7,10 @@
   <div class="green">
     <div class="container ">
       <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2 margin-bottom-lg">
-          <h2 class="textvideo visible-lg">“WE ALL  TOGETHER CAN MAKE A WORLD BETTER”</h2>
-          <h3 class="textvideo visible-sm visible-md">“WE ALL  TOGETHER CAN MAKE A WORLD BETTER”</h3>
+          <h2 class="textvideo visible-lg">WE ALL  TOGETHER CAN MAKE A WORLD BETTER</h2>
+          <h3 class="textvideo visible-sm visible-md">WE ALL  TOGETHER CAN MAKE A WORLD BETTER</h3>
 
-          <h3 class="textvideo visible-xs bg-imagen">“WE ALL  TOGETHER CAN MAKE A WORLD BETTER”</h3>
+          <h3 class="textvideo visible-xs bg-imagen">WE ALL  TOGETHER CAN MAKE A WORLD BETTER</h3>
           <p class="text-center text-standar texto-blanco margin-bottom-lg">If we say that  the internet  pushed the development of this culture, we are not wrong, but if we look beyond our "Smartphones" we can realize that this culture and this industry grew of hand long before the internet and mobile devices were affordable worldwide. It grew thanks to easy access to water both for drinking and hygiene for use in bathrooms.</p>
       </div>
 
