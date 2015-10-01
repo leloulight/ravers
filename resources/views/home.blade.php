911e1f4d46668e8c95252d205808e39db3269447
Luis Abanto <lfam200@hotmail.com>
2015-10-01 09:37:49 -0500

    modificacion video home 4

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -42,7 +42,7 @@
             <video autoplay loop class="fillWidth">
                 <source src="{{ asset('video/heroes_short.mp4') }}" type="video/mp4" />Your browser does not support the video tag. I suggest you upgrade your browser.</video>
             <div class="poster hidden">
-                <img src="http://www.videojs.com/img/poster.jpg" alt="">
+                <img src="{{ asset('img/videoframe.jpg')}}" alt="">
             </div>
         </div>
     </div>
