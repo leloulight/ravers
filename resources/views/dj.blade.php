58e588578d4e2db574f36a8c6d8e53d3af09e0f9
Luis Abanto <lfam200@hotmail.com>
2015-10-03 13:17:58 -0500

    correccion dj

--- resources/views/dj.blade.php
+++ resources/views/dj.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 	<div class="white">
       <div class="container">
-        <h2 class="text-pink visible-lg"><strong>DJ'S</strong> BIRTHDAYS <strong>CHALLENGE</strong></h2>
-        <h3 class="text-pink visible-sm visible-md"><strong>DJ'S</strong> BIRTHDAYS <strong>CHALLENGE</strong></h3>
-				<h3 class="text-pink visible-xs bg-imagen"><strong>DJ'S</strong> BIRTHDAYS <strong>CHALLENGE</strong></h3>
+        <h2 class="text-pink visible-lg"><strong>DJ</strong> BIRTHDAYS <strong>CHALLENGE</strong></h2>
+        <h3 class="text-pink visible-sm visible-md"><strong>DJ</strong> BIRTHDAYS <strong>CHALLENGE</strong></h3>
+				<h3 class="text-pink visible-xs bg-imagen"><strong>DJ</strong> BIRTHDAYS <strong>CHALLENGE</strong></h3>
         
         <div class="row">
         		<div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2">
@@ -21,7 +21,7 @@
         		<p class="pink text-center texto-lg">Subscribe with <br>your email</p>
         	</div>
         	<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-        		<p class="text-center margin-center-lg">If you're a DJ and you want to suppor our causes subscribe and we send you details of how it works and how to subscribe to our calendar.</p>
+        		<p class="text-center margin-center-lg">If you're a fan and you would  love to give a gift to your favorite DJ on the day of his birthday, help him to help as many people as possible. Subscribe if you like the idea and help us to convince them to participate in this great project.</p>
         		<p class="pink text-center texto-lg">Sign a <br>petition.</p>
         	</div>
         	<div class="col-xs-12">
