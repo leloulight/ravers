705782894e88626bb2f21ee263216f030fe6c5dc
Luis Abanto <lfam200@hotmail.com>
2015-10-08 12:52:50 -0500

    donate2

--- resources/views/donate.blade.php
+++ resources/views/donate.blade.php
@@ -3,7 +3,14 @@
 @section('content')
 
 <div class="bg_donate">
-	
+	<div class="container ">
+      <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3 margin-bottom-lg">
+          <h3 class="textvideo visible-lg text-shadow"><strong>3.4 MILLION</strong>  people die yearly by <strong>water</strong> related problems around the world. <br> That's the same <strong>NUMBERS OF RAVERS</strong> who attend to the 20 biggest <strong>EDM FESTIVAL WORLDWIDE.</strong></h3>
+          <h4 class="textvideo visible-sm visible-md text-shadow"><strong>3.4 MILLION</strong>  people die yearly by <strong>water</strong> related problems around the world. <br> That's the same <strong>NUMBERS OF RAVERS</strong> who attend to the 20 biggest <strong>EDM FESTIVAL WORLDWIDE.</strong></h4>
+					<h4 class="textvideo visible-xs bg-imagen text-shadow"><strong>3.4 MILLION</strong>  people die yearly by <strong>water</strong> related problems around the world. <br> That's the same <strong>NUMBERS OF RAVERS</strong> who attend to the 20 biggest <strong>EDM FESTIVAL WORLDWIDE.</strong></h4>
+          <h2 class="text-center texto-blanco text-shadow" id="warriors" >WE CAN DO ANITHING?</h2>
+      </div>
+    </div>  
 </div>
 
 @stop
