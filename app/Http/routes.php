30a6c9adc9403debef125d2e8b72b41595e26625
Luis Abanto <lfam200@hotmail.com>
2015-11-03 06:04:34 -0500

    73

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -70,8 +70,7 @@ Route::get('privacy-policy',['as' => 'policy', 'uses' => 'HomeController@policy'
 
 Route::get('terms-of-use',['as' => 'terms', 'uses' => 'HomeController@terms']);
 
-Route::get('auth', ['as' => 'auth', 'uses' => 'HomeController@auth'];
-});
+Route::get('auth', ['as' => 'auth', 'uses' => 'HomeController@auth']});
 
 Route::get('volunteers/login', 'HomeController@register');
 
