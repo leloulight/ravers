1b96ada6592e92694044836f48f016ac19a000fe
Luis Abanto <lfam200@hotmail.com>
2015-11-04 13:01:37 -0500

    ravers

--- resources/views/ravers.blade.php
+++ resources/views/ravers.blade.php
@@ -6,9 +6,8 @@
   <div class="green">
     <div class="container ">
       <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2 margin-bottom-lg">
-          <h2 class="textvideo visible-lg">WE ALL  TOGETHER CAN MAKE A WORLD BETTER</h2>
-          <h3 class="textvideo visible-sm visible-md">WE ALL  TOGETHER CAN MAKE A WORLD BETTER</h3>
-					<h3 class="textvideo visible-xs bg-imagen">WE ALL  TOGETHER CAN MAKE A WORLD BETTER</h3>
+          <h2 class="textvideo visible-md visible-lg">WE ALL  TOGETHER CAN MAKE A WORLD BETTER</h2>
+          <h3 class="textvideo visible-xs visible-sm">WE ALL  TOGETHER CAN MAKE A WORLD BETTER</h3>
           <p class="text-center text-standar texto-blanco margin-bottom-lg">If we say that  the internet  pushed the development of this culture, we are not wrong, but if we look beyond our "Smartphones" we can realize that this culture and this industry grew of hand long before the internet and mobile devices were affordable worldwide. It grew thanks to easy access to water both for drinking and hygiene for use in bathrooms.</p>
       </div>
 		</div>
@@ -18,15 +17,17 @@
   	<div class="container">
   	    <div class="row margin-bottom-lg">
   	    	<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-  	        <img src="{{ asset('img/wedm.png') }}" alt="Water EDM" width="60%" class="center-block vertical_2" >
-  	      </div>
-  	      <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
-            <h2 class="raver-text text-left visible-lg">Water for the EDM seed</h2>
-            <h3 class="raver-text text-left visible-sm visible-md">Water for the EDM seed</h3>
-            <h3 class="raver-text text-left visible-xs">Water for the EDM seed</h3>
+  	        <img src="{{ asset('img/wedm.png') }}" alt="Water EDM" width="60%" class="center-block vertical_1 visible-md visible-lg" >
+          </div>
+  	      <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8 margin-top-xlg">
+            <h2 class="raver-text text-left visible-md visible-lg">Water for the EDM seed</h2>
+            <h3 class="raver-text text-left visible-xs visible-sm ">Water for the EDM seed</h3>
             
             <p class="text-justify margin-bottom-lg">As we already know in early 70´s, countries like USA and UK, etc  where  the EDM industry was growing, lacked of water scarcity and illness related. 
 						<br><br>Water plays a huge role in the festival scene, as the safety of both attendees and the  environment is a legitimate concern. As the festival industry continually develops into the multi-billion dollar industry it has become, its environmental impact has spiraled out of control.</p>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+            <img src="{{ asset('img/wedm.png') }}" alt="Water EDM" width="60%" class="center-block visible-xs visible-sm" >
           </div>
   	    </div>
   		</div>
@@ -48,27 +49,27 @@
     	        <div class="item active">
     	          <div class="container">
     	            <div class="carousel-caption">
-    	              <div class="col-xs-2">
+    	              <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
     	              	<img src="{{ asset('img/flag/USA.png')}}" alt="flag" width="100%">
     	              	<p class="text-center">Population<br>14.4 mill.</p>
     	              </div>
-    	              <div class="col-xs-2">
+    	              <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
     	              	<img src="{{ asset('img/flag/Brazil.png')}}" alt="flag" width="100%">
     	              	<p class="text-center">Population<br>3.1 mill.</p>
     	              </div>
-    	              <div class="col-xs-2">
+    	              <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
     	              	<img src="{{ asset('img/flag/United-kingdom.png')}}" alt="flag" width="100%">
     	              	<p class="text-center">Population<br>3.1 mill.</p>
     	              </div>
-    	              <div class="col-xs-2">
+    	              <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
     	              	<img src="{{ asset('img/flag/Germany.png')}}" alt="flag" width="100%">
     	              	<p class="text-center">Population<br>2.9 mill.</p>
     	              </div>
-    	              <div class="col-xs-2">
+    	              <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
     	              	<img src="{{ asset('img/flag/Canada.png')}}" alt="flag" width="100%">
     	              	<p class="text-center">Population<br>1.8 mill.</p>
     	              </div>
-    	              <div class="col-xs-2">
+    	              <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
     	              	<img src="{{ asset('img/flag/France.png')}}" alt="flag" width="100%">
     	              	<p class="text-center">Population<br>1.8 mill.</p>
     	              </div>
@@ -78,27 +79,27 @@
     	        <div class="item">
     	          <div class="container">
     	            <div class="carousel-caption">
-    	              <div class="col-xs-2">
+    	              <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
     	              	<img src="{{ asset('img/flag/Italy.png')}}" alt="flag" width="100%">
     	              	<p class="text-center">Population<br>1.6 mill.</p>
     	              </div>
-    	              <div class="col-xs-2">
+    	              <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
     	              	<img src="{{ asset('img/flag/Japan.png')}}" alt="flag" width="100%">
     	              	<p class="text-center">Population<br>1.2 mill.</p>
     	              </div>
-    	              <div class="col-xs-2">
+    	              <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
     	              	<img src="{{ asset('img/flag/Spain.png')}}" alt="flag" width="100%">
     	              	<p class="text-center">Population<br>1.0 mill.</p>
     	              </div>
-    	              <div class="col-xs-2">
+    	              <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
     	              	<img src="{{ asset('img/flag/Australia.png')}}" alt="flag" width="100%">
     	              	<p class="text-center">Population<br>0.9 mill.</p>
     	              </div>
-    	              <div class="col-xs-2">
+    	              <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
     	              	<img src="{{ asset('img/flag/Russia.png')}}" alt="flag" width="100%">
     	              	<p class="text-center">Population<br>0.8 mill.</p>
     	              </div>
-    	              <div class="col-xs-2">
+    	              <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
     	              	<img src="{{ asset('img/flag/Netherlands.png')}}" alt="flag" width="100%">
     	              	<p class="text-center">Population<br>0.7 mill.</p>
     	              </div>
@@ -108,27 +109,27 @@
     	        <div class="item">
     	          <div class="container">
     	            <div class="carousel-caption">
-    	              <div class="col-xs-2">
+    	              <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
     	              	<img src="{{ asset('img/flag/Mexico.png')}}" alt="flag" width="100%">
     	              	<p class="text-center">Population<br>0.7 mill.</p>
     	              </div>
-    	              <div class="col-xs-2">
+    	              <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
     	              	<img src="{{ asset('img/flag/India.png')}}" alt="flag" width="100%">
     	              	<p class="text-center">Population<br>0.6 mill.</p>
     	              </div>
-    	              <div class="col-xs-2">
+    	              <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
     	              	<img src="{{ asset('img/flag/South-korea.png')}}" alt="flag" width="100%">
     	              	<p class="text-center">Population<br>0.5 mill.</p>
     	              </div>
-    	              <div class="col-xs-2">
+    	              <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
     	              	<img src="{{ asset('img/flag/Belgium.png')}}" alt="flag" width="100%">
     	              	<p class="text-center">Population<br>0.5 mill.</p>
     	              </div>
-    	              <div class="col-xs-2">
+    	              <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
     	              	<img src="{{ asset('img/flag/Czech-Republic.png')}}" alt="flag" width="100%">
     	              	<p class="text-center">Population<br>0.5 mill.</p>
     	              </div>
-    	              <div class="col-xs-2">
+    	              <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
     	              	<img src="{{ asset('img/flag/Sweden.png')}}" alt="flag" width="100%">
     	              	<p class="text-center">Population<br>0.4 mill.</p>
     	              </div>
@@ -138,27 +139,27 @@
     	        <div class="item">
     	          <div class="container">
     	            <div class="carousel-caption">
-    	              <div class="col-xs-2">
+    	              <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
     	              	<img src="{{ asset('img/flag/China.png')}}" alt="flag" width="100%">
     	              	<p class="text-center">Population<br>0.4 mill.</p>
     	              </div>
-    	              <div class="col-xs-2">
+    	              <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
     	              	<img src="{{ asset('img/flag/Chile.png')}}" alt="flag" width="100%">
     	              	<p class="text-center">Population<br>0.3 mill.</p>
     	              </div>
-    	              <div class="col-xs-2">
+    	              <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
     	              	<img src="{{ asset('img/flag/Turkey.png')}}" alt="flag" width="100%">
     	              	<p class="text-center">Population<br>0.3 mill.</p>
     	              </div>
-    	              <div class="col-xs-2">
+    	              <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
     	              	<img src="{{ asset('img/flag/Denmark.png')}}" alt="flag" width="100%">
     	              	<p class="text-center">Population<br>0.2 mill.</p>
     	              </div>
-    	              <div class="col-xs-2">
+    	              <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
     	              	<img src="{{ asset('img/flag/Norway.png')}}" alt="flag" width="100%">
     	              	<p class="text-center">Population<br>0.2 mill.</p>
     	              </div>
-    	              <div class="col-xs-2">
+    	              <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
     	              	<img src="{{ asset('img/flag/New-zealand.png')}}" alt="flag" width="100%">
     	              	<p class="text-center">Population<br>0.2 mill.</p>
     	              </div>
@@ -168,15 +169,15 @@
     	        <div class="item">
     	          <div class="container">
     	            <div class="carousel-caption">
-    	              <div class="col-xs-2">
+    	              <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
     	              	<img src="{{ asset('img/flag/Argentina.png')}}" alt="flag" width="100%">
     	              	<p class="text-center">Population<br>0.2 mill.</p>
     	              </div>
-    	              <div class="col-xs-2">
+    	              <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
     	              	<img src="{{ asset('img/flag/United-arab-emirates.png')}}" alt="flag" width="100%">
     	              	<p class="text-center">Population<br>0.1 mill.</p>
     	              </div>
-    	              <div class="col-xs-2">
+    	              <div class="col-xs-6 col-sm-6 col-md-2 col-lg-2">
     	              	<img src="{{ asset('img/flag/South-africa.png')}}" alt="flag" width="100%">
     	              	<p class="text-center">Population<br>0.1 mill.</p>
     	              </div>
@@ -193,13 +194,13 @@
       <div class="container">
         <div class="row">
         	<div class="col-xs-12 col-sm-12 col-md-7 col-lg-7">
-            <h2 class="water text-left visible-lg">How much water we drink?</h2>
-            <h3 class="water text-left visible-sm visible-md">How much water we drink?</h3>
-            <h3 class="water text-left visible-xs">How much water we drink?</h3>
-            <p class="text-justify texto-blanco margin-bottom-xlg">According MayoClinic.org is estimated that women should consume 2.2 liters of water daily, while it is estimated that men should consume about three liters a day. Most festivals sell plastic water bottles 20 fluid ounces (equivalent to 0.591 liters), which means that every raver should drink at least four bottles of water each day. <br><br>A study at the Glastonbury Festival of water consumed, said they had a total audience of 730,000 in five days. Glastonbury uses 10,000 m3 during the event. So 10000000 (Lts) / 730000 (Hearing) = 13.69 l / 4 nights of festival = 3.4 liters of water every day or 6 bottles of water each day festival</p>
+            <h2 class="water text-left visible-md visible-lg">How much water we drink?</h2>
+            <h3 class="water text-left visible-xs visible-sm ">How much water we drink?</h3>
+            <p class="text-justify texto-blanco">According MayoClinic.org is estimated that women should consume 2.2 liters of water daily, while it is estimated that men should consume about three liters a day. Most festivals sell plastic water bottles 20 fluid ounces (equivalent to 0.591 liters), which means that every raver should drink at least four bottles of water each day. <br><br>A study at the Glastonbury Festival of water consumed, said they had a total audience of 730,000 in five days. Glastonbury uses 10,000 m3 during the event. So 10000000 (Lts) / 730000 (Hearing) = 13.69 l / 4 nights of festival = 3.4 liters of water every day or 6 bottles of water each day festival</p>
           </div>
-	        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-	          <img src="{{ asset('img/water.png') }}" alt="Water Planet" width="60%" class="center-block vertical_2" >
+	        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 margin-bottom-xlg">
+	          <img src="{{ asset('img/water.png') }}" alt="Water Planet" width="60%" class="center-block vertical_2 visible-md visible-lg" >
+            <img src="{{ asset('img/water.png') }}" alt="Water Planet" width="60%" class="center-block visible-xs visible-sm" >
 	        </div>
       	</div>
     	</div>
@@ -212,7 +213,7 @@
 	          <h2 class="textvideo_no_bottom visible-lg">Our environmental impact <br><small>*Recyclables</small></h2>
 	          <h3 class="textvideo_no_bottom visible-sm visible-md">Our environmental impact <br><small>*Recyclables</small></h3>
 						<h3 class="textvideo_no_bottom visible-xs bg-imagen">Our environmental impact <br><small>*Recyclables</small></h3>
-	          <p class="text-center text-standar texto-blanco margin-bottom-lg">A raver consume on average 6 bottles per day, multiplied by 4 days = 24 plastic bottles by raver. <br>This multiplied by the total population of each event is a shocking number of plastic bottles.</p>
+	          <p class="text-center text-standar texto-blanco margin-bottom-lg">A raver consume on average 6 bottles per day, multiplied by 7 days = 28 plastic bottles by raver. <br>This multiplied by the total population of each event is a shocking number of plastic bottles.</p>
 	      	</div>
       	</div>
       	<div class="row">
@@ -234,9 +235,9 @@
       		</div>
       	</div>
       	<div class="row margin-bottom-lg">
-      		<div class="col-xs-1"><img src="{{ asset('img/bottle.png') }}" alt="Barril" width="30%" class="center-block"></div>
+      		<div class="col-xs-1"><img src="{{ asset('img/bottle.png') }}" alt="Barril" width="30%" class="center-block visible-md visible-lg"></div>
       		<div class="col-xs-10"><p class="text-center texto-blanco margin-top">1 BOTTLE WITH WATER IS 2000 TIMES MORE EXPENSIVE THAN A GLASS WITH WATER !!!</p></div>
-      		<div class="col-xs-1"><img src="{{ asset('img/bottle.png') }}" alt="Barril" width="30%" class="center-block"></div>
+      		<div class="col-xs-1"><img src="{{ asset('img/bottle.png') }}" alt="Barril" width="30%" class="center-block visible-md visible-lg"></div>
       	</div>
         
       </div>
@@ -255,7 +256,8 @@
 						<br>The equivalent: 1,000,000 beers / weight of 100 elephants</p>  
           </div> 
           <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-            <img src="{{ asset('img/carpa.png') }}" alt="Tents" width="60%" class="center-block vertical margin-bottom-lg" >
+            <img src="{{ asset('img/carpa.png') }}" alt="Tents" width="60%" class="center-block vertical margin-bottom-lg visible-md visible-lg" >
+            <img src="{{ asset('img/carpa.png') }}" alt="Tents" width="60%" class="center-block margin-bottom-lg visible-xs visible-sm" >
           </div>
     		</div>
 				<div class="row">
