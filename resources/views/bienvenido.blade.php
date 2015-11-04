645108c0a20abc50adbeabdb4b76d99dd421d18e
Luis Abanto <lfam200@hotmail.com>
2015-11-03 23:05:16 -0500

    bienvenidos

--- resources/views/bienvenido.blade.php
+++ resources/views/bienvenido.blade.php
@@ -7,10 +7,9 @@
                 <div class="col-sm-12 margin-top-lg margin-bottom-xlg">
                     <div class="col-sm-6 col-sm-offset-3">
                         <h3 class="texto-blanco text-uppercase">Bienvenido {{ $user }}</h3>
-                        <p class="text-justify texto-blanco">Para que disfrutes al 100% la experiencia de la plataforma necesitamos que registres un e-mail válido.<br>De esta manera podremos darte una mejor atención, enviarte notificaciones del curso, avisarte cuando esté disponible el examen de certificación y algunas sorpresas que tendremos para tí.</p>
+                        <p class="text-justify texto-blanco">Estamos trabajando para brindarte la mejor experiencia, te mantendremos informado de nuestro avanze.
+                            <br/> Gracias por registrarte con nosotros..</p>
                         <br>
-                        <button type="submit" class="btn btn-success">Agregar Email</button>
-                        <br/>
                         <img src="{{ asset('img/isologo.png') }}" width="40%" alt="" class="center-block img-responsive"/>
                         <h2 class="text-center texto-blanco text-uppercase">international ravers charity</h2>
                     </div>
