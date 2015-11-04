54776bcb598abbecaf59fdf5726e2faf331df500
Luis Abanto <lfam200@hotmail.com>
2015-11-03 19:25:52 -0500

    migraa

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('handle');
-            $table->integer('twitter_id');
+            $table->date('birthday');
             $table->string('avatar');
             $table->string('email')->unique();
             $table->string('password', 60);
